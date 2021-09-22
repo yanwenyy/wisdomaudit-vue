89b7375af621aa278410daf5eb378b49934a310e
jiashihua <1535288457@qq.com>
2021-09-22 17:29:50 +0800

    前端bug修改

--- src/views/audit/projectmanagement/index.vue
+++ src/views/audit/projectmanagement/index.vue
@@ -56,51 +56,91 @@
       </el-table-column>
     </el-table>
 
-    <el-dialog title="新增" :visible.sync="addDialogVisible" width="50%">
+    <el-dialog title="新增" :visible.sync="addDialogVisible" width="50%"  @close="addDialogClosed">
       <div class="addForm">
-        <el-form label-width="100px">
+        <el-form
+          label-width="100px"
+          :rules="rules"
+          :model="addProjectManagement"
+          ref="form"
+        >
           <el-row>
-            <el-form-item label="项目编号:">
-              <el-input placeholder="hk123456789"> </el-input>
+            <el-form-item label="项目编号:" prop="itemNo">
+              <el-input
+                placeholder=""
+                v-model="addProjectManagement.itemNo"
+              ></el-input>
             </el-form-item>
           </el-row>
           <el-row>
-            <el-form-item label="项目分类:">
-              <el-select placeholder="专项"></el-select>
+            <el-form-item label="项目分类:" prop="itemClassfication">
+              <el-select
+                placeholder="请选择"
+                v-model="addProjectManagement.itemClassfication"
+              >
+                <el-option label="是" value="shi"></el-option>
+                <el-option label="否" value="fou"></el-option>
+              </el-select>
             </el-form-item>
           </el-row>
           <el-row>
-            <el-form-item label="项目名称:">
-              <el-input placeholder="请输入"> </el-input>
+            <el-form-item
+              label="项目名称:"
+              prop="projectName"
+            >
+              <el-input
+                placeholder="请输入"
+                v-model="addProjectManagement.projectName"
+              >
+              </el-input>
             </el-form-item>
           </el-row>
           <el-row>
-            <el-form-item label="项目负责人:">
-              <el-select placeholder="请输入"></el-select>
+            <el-form-item
+              label="项目负责人:"
+              prop="projectLeader"
+            >
+              <el-select
+                placeholder="请选择"
+                v-model="addProjectManagement.projectLeader"
+              >
+                <el-option label="是" value="shi"></el-option>
+                <el-option label="否" value="fou"></el-option>
+              </el-select>
             </el-form-item>
           </el-row>
           <el-row>
-            <el-form-item label="审计期间:">
-              <el-date-picker type="date" placeholder="请选择"></el-date-picker>
+            <el-form-item
+              label="审计期间:"
+              prop="projectPeriod"
+            >
+              <el-date-picker
+                type="date"
+                placeholder="请选择"
+                v-model="addProjectManagement.projectPeriod"
+              ></el-date-picker>
             </el-form-item>
-            <el-form-item label="设置组长"> 123 </el-form-item>
-            <el-table :data="leaderData" style="width: 100%" border>
-              <el-table-column label="项目编号" prop="projectItem" width="150">
+            <el-form-item label="设置组长"></el-form-item>
+            <el-table :data="leaderData" style="width: 100%" border class="projectTable">
+              <el-table-column label="项目编号" prop="projectItem" width="110">
               </el-table-column>
-              <el-table-column prop="auditee" label="被审计单位">
+              <el-table-column prop="auditee" label="被审计单位" width="330">
                 <template scope="scope">
-                  <el-input
+                  <el-form-item prop="auditee">
+                  <el-input 
                     placeholder="请输入"
                     v-model="scope.row.auditee"
                     class="auditeeInput"
                   ></el-input>
+                  </el-form-item>
                 </template>
               </el-table-column>
-              <el-table-column prop="role" label="角色" width="100">
+              <el-table-column prop="role" label="角色" width="60">
               </el-table-column>
-              <el-table-column prop="personCharge" label="负责人">
+              <el-table-column prop="personCharge" label="负责人" width="330">
                 <template scope="scope">
-                  <el-select
+                  <el-form-item prop="personCharge">
+                     <el-select
                     placeholder="请选择"
                     class="auditeeInput"
                     v-model="scope.row.personCharge"
@@ -108,6 +148,7 @@
                     <el-option label="是" value="shi"></el-option>
                     <el-option label="否" value="fou"></el-option>
                   </el-select>
+                  </el-form-item>
                 </template>
               </el-table-column>
               <el-table-column label="操作" width="80">
@@ -129,6 +170,11 @@
             </div>
           </el-row>
         </el-form>
+
+         <div class="stepBtn">
+          <el-button @click="addDialogVisible = false">取消</el-button>
+          <el-button class="nextBtn" @click="nextBtn">确认</el-button>
+        </div>
       </div>
     </el-dialog>
   </div>
@@ -200,6 +246,44 @@ export default {
           personCharge: "",
         },
       ],
+      //新增项目管理的表单
+      addProjectManagement: {
+        itemNo: "",
+        itemClassfication: "",
+        projectName: "",
+        projectLeader: "",
+        projectPeriod: "",
+      },
+      // 新增的表单验证
+      rules: {
+        itemNo: [
+          { required: true, message: "请输入项目编号", trigger: "blur" },
+          {
+            max: 10,
+            message: "项目编号应在10个字符之内",
+            trigger: "change",
+          },
+        ],
+        itemClassfication: [
+          { required: true, message: '请选择项目分类', trigger: "change"}
+        ],
+        projectName: [
+          { required: true, message: "请填写项目名称", trigger: "blur" },
+          { max: 10, message: "项目名称在10个字符之内", trigger: "change" },
+        ],
+        projectLeader:[
+           { required: true, message: "请选择项目负责人", trigger: "change" },
+        ],
+        projectPeriod :[
+           { required: true, message: "请选择审计期间", trigger: "change" },
+        ],
+        auditee:[
+           { required: true, message: "请输入被审计单位", trigger: "blur" },
+        ],
+        personCharge:[
+           { required: true, message: "请选择负责人", trigger: "change" },
+        ]
+      },
     };
   },
   methods: {
@@ -217,6 +301,15 @@ export default {
     },
     deleteRow(index, rows) {
       rows.splice(index, 1);
+    },
+    nextBtn() {
+      this.$refs["form"].validate((valid) => {
+        console.log(valid);
+      })
+    },
+    //监听添加用户对话框的关闭事件
+    addDialogClosed() {
+      this.$refs["form"].resetFields();
     },
   },
 };
@@ -243,7 +336,10 @@ export default {
   .auditeeInput {
     position: relative;
     top: 0px !important;
+    left: -110px !important;
+    height: 100%;
     width: 200px;
+    height: 64px;
   }
   .addIcon {
     background-color: #fff;
@@ -256,4 +352,26 @@ export default {
     margin-bottom: 5%;
   }
 }
+.stepBtn {
+  /* border: 1px solid red; */
+  // margin-top: 5%;
+  text-align: center;
+  .nextBtn {
+  background: #508ce6 !important;
+  color: #fff;
+}
+}
+
+</style>
+<style scoped>
+  .addForm /deep/ .el-form-item__error{
+    position: absolute;
+    top: -70%;
+    left: 52%;
+  }
+  .projectTable /deep/ .el-form-item__error{
+    position: absolute;
+    top: 15%;
+    left: 45%;
+  }
 </style>
