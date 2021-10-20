5486957a900231881cb676f1067732d847665460
luanwuxu <3379663227@qq.com>
2021-10-20 09:59:25 +0800

    request

--- src/utils/request.js
+++ src/utils/request.js
@@ -49,7 +49,7 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.status !== 0) {
+    if (res.code !== 0 && res.status !== 0) {
       Message({
         message: res.msg || 'Error',
         type: 'error',
