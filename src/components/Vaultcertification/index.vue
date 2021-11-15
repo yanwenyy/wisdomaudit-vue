82e8ff16084a673885e926e4df0a71dce724981b
luanwuxu <3379663227@qq.com>
2021-11-15 14:39:28 +0800

    版本号加金库

--- src/components/Vaultcertification/index.vue
+++ src/components/Vaultcertification/index.vue
@@ -16,44 +16,49 @@
           label-width="120px"
         >
           <el-form-item label="访问方式">
-            <el-select v-model="form.region" placeholder="请选择访问方式">
+            <!-- <el-select v-model="form.region" placeholder="请选择访问方式">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
-            </el-select>
+            </el-select> -->
+            <el-input v-model="form.region" disabled></el-input>
           </el-form-item>
           <el-form-item label="申请人">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="form.people" disabled></el-input>
           </el-form-item>
           <el-form-item label="截止时间">
-            <el-date-picker
-              v-model="formtime"
-              type="datetime"
-              placeholder="选择截止时间"
-            >
-            </el-date-picker>
+            <el-input v-model="form.endTime" disabled></el-input>
           </el-form-item>
           <el-form-item label="审批人">
-            <el-select v-model="form.region" placeholder="请选择审批人">
-              <el-option label="区域一" value="shanghai"></el-option>
-              <el-option label="区域二" value="beijing"></el-option>
+            <el-select
+              v-model="form.approverAccount"
+              placeholder="请选择审批人"
+            >
+              <el-option
+                v-for="(item, index) in approvers"
+                :key="'appa' + index"
+                :label="item.name"
+                :value="item.name"
+              ></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="常用申请原因">
+          <!-- <el-form-item label="常用申请原因">
             <el-select v-model="form.region" placeholder="请选择常用申请原因">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
             </el-select>
-          </el-form-item>
+          </el-form-item> -->
           <el-form-item label="申请原因">
-            <el-input type="textarea" v-model="form.desc" placeholder="此内容会被审计请慎重填写"></el-input>
+            <el-input
+              type="textarea"
+              v-model="form.cerReason"
+              placeholder="此内容会被审计请慎重填写"
+            ></el-input>
           </el-form-item>
         </el-form>
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button @click="vaultVisible = false">取 消</el-button>
-        <el-button type="primary" @click="sortinfoVisible = true"
-          >下一步</el-button
-        >
+        <el-button type="primary" @click="opensort()">下一步</el-button>
       </span>
     </el-dialog>
     <el-dialog
@@ -63,7 +68,12 @@
       center
       ><span class="VaultTitle">远程授权</span>
       <div>
-        <el-form ref="form" :model="sortform" label-width="180px" class="vault-form">
+        <el-form
+          ref="sortform"
+          :model="sortform"
+          label-width="180px"
+          class="vault-form"
+        >
           <el-form-item label="请输入动态验证码">
             <el-input v-model="sortform.info"></el-input>
           </el-form-item>
@@ -71,38 +81,130 @@
       </div>
       <span slot="footer" class="dialog-footer">
         <el-button @click="sortinfoVisible = false">取 消</el-button>
-        <el-button type="primary" @click="sortinfoVisible = false"
-          >确定</el-button
-        >
+        <el-button type="primary" @click="secondauth()">确定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
-  props:['vaultV','sceneId'],
+  props: [
+    "vaultV",
+    "sceneId",
+    "approvers",
+    "maxTime",
+    "account",
+    "appSessionId",
+    "dqtime",
+  ],
   data() {
     return {
       vaultVisible: false,
       sortinfoVisible: false,
       form: {
-        region: "",
-        name: "",
-        desc: "",
+        region: "动态口令（短信）",
+        approverAccount: "",
+        people: "",
+        cerReason: "",
+        endTime: "",
+        beginTime: "",
       },
-      formtime: "",
       sortform: {
         info: "",
       },
     };
   },
-  watch:{
-    vaultV(val, oldVal){
-      console.log(this.vaultV)
-      this.vaultVisible = val
-     },
+  watch: {
+    vaultV(val, oldVal) {
+      this.vaultVisible = val;
+    },
+    vaultVisible(val, oldVal) {
+      this.changevault(val);
+    },
+    dqtime(val, oldVal) {
+      //获取当前时间并打印
+      let beginhh = this.dqtime.getHours();
+      let targetday = this.dqtime.getTime() + 1000 * 60 * 60 * this.maxTime;
+      this.dqtime.setTime(targetday);
+      let _this = this;
+      let yy = this.dqtime.getFullYear();
+      let mm = this.dqtime.getMonth() + 1;
+      let dd = this.dqtime.getDate();
+      let hh = this.dqtime.getHours();
+      let mf =
+        this.dqtime.getMinutes() < 10
+          ? "0" + this.dqtime.getMinutes()
+          : this.dqtime.getMinutes();
+      let ss =
+        this.dqtime.getSeconds() < 10
+          ? "0" + this.dqtime.getSeconds()
+          : this.dqtime.getSeconds();
+      let getbegintime =
+        yy + "-" + mm + "-" + dd + " " + beginhh + ":" + mf + ":" + ss;
+      let getendtime =
+        yy + "-" + mm + "-" + dd + " " + hh + ":" + mf + ":" + ss;
+      console.log(getbegintime);
+      console.log(getendtime);
+      this.form.beginTime = getbegintime;
+      this.form.endTime = getendtime;
+    },
   },
-  methods: {},
+  created() {
+    let p = sessionStorage.getItem("store");
+    this.form.people = JSON.parse(p).name;
+  },
+  methods: {
+    changevault(val) {
+      this.$emit("changevault", val);
+    },
+    opensort() {
+      axios({
+        method: "post",
+        url: `/wisdomaudit/remoteFirstAuth`,
+        data: {
+          account: this.account,
+          sceneId: this.sceneId,
+          approverAccount: this.form.approverAccount, //审批人主账号
+          beginTime: this.form.beginTime,
+          endTime: this.form.endTime,
+          appSessionId: this.appSessionId,
+          cerReason: this.form.cerReason, //原因
+        },
+      }).then((resp) => {
+        this.sortinfoVisible = true;
+      });
+    },
+    secondauth() {
+      axios({
+        method: "post",
+        url: `/wisdomaudit/remoteSecondAuth`,
+        data: {
+          account: this.account,
+          sceneId: this.sceneId,
+          approverAccount: this.form.approverAccount, //审批人主账号
+          beginTime: this.form.beginTime,
+          endTime: this.form.endTime,
+          appSessionId: this.appSessionId,
+          isCommonManager: "y",
+          dynamicCode: this.sortform.info,
+        },
+      }).then((resp) => {
+        this.vaultVisible = false;
+        this.sortinfoVisible = false;
+        this.sortform.info = "";
+        if (resp.data.code == 0) {
+          this.$message({
+            message: "认证成功",
+            type: "success",
+          });
+          this.$emit("download");
+        } else {
+          this.$message.error(resp.data.msg);
+        }
+      });
+    },
+  },
 };
 </script>
 <style lang="scss">
