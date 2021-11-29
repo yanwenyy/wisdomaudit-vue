0426ea36c33fbf9be90ac081857355ae019f5df0
huangyilei@aspirecn.com <838043724@gmail.com>
2021-11-29 16:50:02 +0800

    fix：用户管理和权限管理按钮位置调整

--- src/views/systemmanagement/index.vue
+++ src/views/systemmanagement/index.vue
@@ -4,7 +4,7 @@
       <div class="two">
         <div class="projectTab">
           <el-row class="titleMes" type="flex" justify="space-between">
-            <el-col :span="7">
+            <el-col :span="6">
               <div class="search">
                 <el-input
                   placeholder="请输入角色名称搜索"
@@ -14,12 +14,11 @@
                 <el-button class="search_icon" @click="searchName">
                   <i class="el-icon-search" style="color: #fff"></i>
                 </el-button>
-<!--                <el-button type="primary" @click="searchName">搜索</el-button>-->
-                <el-button type="primary" @click="new_add()">新增</el-button>
               </div>
             </el-col>
-<!--            <el-col :span="2">-->
-<!--            </el-col>-->
+            <el-col :span="2">
+              <el-button type="primary" @click="new_add()">新增</el-button>
+            </el-col>
 
             <!-- <el-col :span="1.5">
               <el-button type="primary"
@@ -285,7 +284,6 @@ export default {
 .search_icon {
   position: absolute;
   top: 0;
-  right: 70px;
   background: #1371cc;
   width: 37px;
   height: 37px;
