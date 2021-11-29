7322e3fb9bec472b7401df16ee33eccd1c8419a4
luokai <luokai@aspirecn.com>
2021-11-29 16:36:27 +0800

    完成

--- src/views/audit/riskScan/enterprise/index.vue
+++ src/views/audit/riskScan/enterprise/index.vue
@@ -125,7 +125,6 @@ export default {
           getdataAuditApi(result.data.token).then((res)=>{
 
            if (res.status== 'success') {
-            //  this.formdates='http:10.19.206.196:8088/WebReport/decision/view/form?viewlet=vendor/zhuowang/test.cpt&ref_t=design&ref_c=d6740dbd-0279-40d0-b361-3cc1adb80d35'
             this.formdates=res.url.replace('&amp;','&')
              getTypes("area=2").then((rem) => {
               this.options = rem.data;
