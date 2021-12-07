54744bde6c924508e4680a56a5215f876827bdc6
yanwenyy <18738390058>
2021-12-07 22:29:31 +0800

    修改bug

--- src/views/audit/projectworkbench/index.vue
+++ src/views/audit/projectworkbench/index.vue
@@ -1176,6 +1176,7 @@ export default {
         this.getInitProject(this.queryProject);
         this.project_more();
         if (from == 'home') {
+          this.get_user();
           this.active_project = this.queryInfo.projectId;
           this.defaultActive = this.queryInfo.index;
           this.index = this.queryInfo.index;
