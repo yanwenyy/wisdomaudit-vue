af06e07939b04fd2d0cbf42ffa4538e1569857d7
luokai <luokai@aspirecn.com>
2021-12-23 17:37:34 +0800

    ie兼容

--- src/views/audit/riskScan/personal/index.vue
+++ src/views/audit/riskScan/personal/index.vue
@@ -80,7 +80,7 @@
      <iframe :src="formdates" frameborder="0" width="100%"  height="800px" ></iframe>
     </div>
 
-     
+
 
     </div>
   </div>
@@ -112,11 +112,11 @@ export default {
   },
 
   created() {
-
+   this.gettapylist();
   },
 
   mounted() {
-        // this.gettapylist();
+ 
   },
 
   methods: {
